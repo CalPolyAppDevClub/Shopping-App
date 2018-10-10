01604c82c8bc9883ba2d74dac26110d915e4955c
Ethan Abrams <ethana56@Ethans-MacBook-Pro.local>
2018-10-09 18:21:51 -0700

    first commit

---
+++ ShoppingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Ethan Abrams on 10/9/18.
+//  Copyright © 2018 App Dev Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ShoppingAppTests/ShoppingAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ShoppingAppTests.swift
+//  ShoppingAppTests
+//
+//  Created by Ethan Abrams on 10/9/18.
+//  Copyright © 2018 App Dev Club. All rights reserved.
+//
+
+import XCTest
+@testable import ShoppingApp
+
+class ShoppingAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
