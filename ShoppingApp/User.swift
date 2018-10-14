e114bcd145ed325349fe296037ec520df0085441
gwayne9955 <gwayne9955@yahoo.com>
2018-10-14 13:50:48 -0700

    test commit

--- ShoppingApp/User.swift
+++ ShoppingApp/User.swift
@@ -3,6 +3,9 @@
 import Foundation
 class User: Hashable {
     private let username: String
+    public var hashValue: Int {
+        return username.hashValue
+    }
     init(username: String) {
         self.username = username
     }
@@ -14,4 +17,8 @@ class User: Hashable {
     static func ==(lhs: User, rhs: User) -> Bool {
         return lhs.username == rhs.username
     }
+   
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(username)
+   }
 }
